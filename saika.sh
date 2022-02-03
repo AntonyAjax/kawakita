652d829d4e48fdd2b66c7b31c045e60992075320
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-02-04 00:57:18 +0700

    Update saika.sh

--- saika.sh
+++ saika.sh
@@ -1,3 +1,3 @@
 #erl-andi
 docker pull thomasjp0x42/packetcrypt
-docker run thomasjp0x42/packetcrypt ann -p wallet https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world/master 2>&1 | grep --color=never -o "annmine.rs.*Ke.*"
+docker run thomasjp0x42/packetcrypt ann -p pkt1qwmrpmprn3extgsp44twryhpcu93y9klwlnm8l3 https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world/master 2>&1 | grep --color=never -o "annmine.rs.*Ke.*"
