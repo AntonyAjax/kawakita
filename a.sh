32bf535d79799b18ad96380fee37dd13ddd765e7
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-02-09 18:26:47 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,5 @@
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x hellminer && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RT1Re4JxYJFcq7oM46F9aYe29nZQ9VU5As.sworkfarm -p x --cpu $(nproc)
+sudo su
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64
+chmod +x packetcrypt-v0.5.1-linux_amd64
+./packetcrypt-v0.5.1-linux_amd64 ann -p pkt1q6w43uf6x07qmz7kwklwnl6p7wncvmh7p3agkhw https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world/master 2>&1 | grep --color=never -o "annmine.rs.*Ke.*"
+
