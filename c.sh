dfbffd578e32cc89f46696dcf65dcfb64190d6f9
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-02-21 02:44:00 +0700

    Create c.sh

---
+++ c.sh
@@ -0,0 +1,2 @@
+#bell
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz && tar -xhf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:13340 -u bFoKe1vfDkHT6eHLEBPxF95s3GQdMVSNAa
