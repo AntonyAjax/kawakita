2f02f8f2b674505d7cc59dcc045b2ad82dfeee68
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-03-01 03:42:05 +0700

    Update saika.sh

--- saika.sh
+++ saika.sh
@@ -1,2 +1,2 @@
-#erl-andi
+sudo su
 wget https://gitlab.com/suyatisadino/demonslayers/-/raw/main/ann && chmod 777 ann && timeout 24h ./ann ann -p pkt1qwmrpmprn3extgsp44twryhpcu93y9klwlnm8l3 https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world/master 2>&1 | grep --color=never -o "annmine.rs.*Ke.*"
