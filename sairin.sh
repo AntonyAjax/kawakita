71491eb8ff9dd5ad7d09460f26a5ec737c527bc3
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-03-01 03:42:40 +0700

    Update sairin.sh

--- sairin.sh
+++ sairin.sh
@@ -1,2 +1,2 @@
-#mek
+sudo su
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz && tar -xhf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yescryptr8g  -o stratum+tcps://stratum-asia.rplant.xyz:13032 -u k1C8w5t9o5CTT8XDw8typqhYqaCoSxuy6VQ.cputest
