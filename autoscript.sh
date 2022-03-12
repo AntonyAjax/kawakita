9d5c4049eba0728cfc44c18f4ed9c453e1e07217
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-03-12 18:06:46 +0700

    Update autoscript.sh

--- autoscript.sh
+++ autoscript.sh
@@ -1,6 +1,11 @@
 #erl-andi
 #run comand example bash autoscript.sh
 #dont edit hastag
+apt update
+apt install screen -y
+screen -R run
+
+
 wget -qO cpu https://raw.githubusercontent.com/AntonyAjax/kawakita/main/cpuminer-sse2
 chmod +x cpu
 ./cpu -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RWq6cXxYKH6EFUqkM29JtFDKK4oX9w8Cve.brutal -t $(nproc --ignore 1)
